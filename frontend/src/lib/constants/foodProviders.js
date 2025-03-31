153c8f6e5ba3f870b1e5acb31831ab4181fb9a7c
Zoltan Spagina <zoltan.spagina@nokia.com>
2025-03-31 18:41:24 +0200

    Fix enum value on frontend

--- frontend/src/lib/constants/foodProviders.js
+++ frontend/src/lib/constants/foodProviders.js
@@ -1,6 +1,6 @@
 export const FoodProvider = {
-    CITY_FOOD: {label: "CityFood", value: "CITY_FOOD"},
-    INTER_FOOD: {label: "Interfood", value: "INTER_FOOD"},
+    CITY_FOOD: {label: "Cityfood", value: "cityfood"},
+    INTER_FOOD: {label: "Interfood", value: "interfood"},
 };
 export const foodProviderList = Object.values(FoodProvider);
 
