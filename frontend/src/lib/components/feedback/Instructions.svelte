541be8b97c46aaf4fa7b5f53a99faedeeef3af0c
Zoltan Spagina <zoltan.spagina@nokia.com>
2025-04-05 12:13:39 +0200

    Minor improvements on instructions

--- frontend/src/lib/components/feedback/Instructions.svelte
+++ frontend/src/lib/components/feedback/Instructions.svelte
@@ -1,11 +1,11 @@
 <div class="box has-background-light p-4 is-rounded shadow">
     <h2 class="title is-5 has-text-centered">📋 Need help?</h2>
     <ul class="content">
-        <li>✅ Add your <strong>nutritional constraints</strong> (calories, protein, carbs, fats).</li>
+        <li>🎯 Add your <strong>nutritional goals</strong> (calories, protein, carbs, fats).</li>
         <li>⚖️ <strong>You only need to enter either the minimum or maximum values.</strong></li>
         <li>📅 <strong>Select a day</strong> for which you want to order.</li>
-        <li>🍽️ The system will generate a <strong>list of food</strong> that fit your requirements.</li>
-        <li>🚫 If you have foods you dislike, <strong>add them to the "Do Not Like" field</strong> to filter them out.</li>
+        <li>📋 The app will generate a <strong>meal plan</strong> that fit your requirements.</li>
+        <li>🚫 If you have foods you dislike, <strong>add them to the "Foods You dislike" field</strong> to filter them out.</li>
         <li>💰 The <strong>generator prioritizes minimizing price</strong> while still fitting your nutritional constraints.</li>
         <li>⚠️ The <strong>more constraints you add, the harder it is</strong> to find a cheap meal plan.</li>
     </ul>
