a156b8ce8ed85a926276666785a35aae62c44622
Zoltan Spagina <zoltan.spagina@nokia.com>
2025-04-16 21:25:47 +0200

    Properly clean up docker

--- update_deployment.sh
+++ update_deployment.sh
@@ -12,6 +12,6 @@ echo "Rebuilding and starting new containers..."
 docker compose up -d --build --remove-orphans
 
 echo "Cleaning up old Docker images..."
-docker system prune -a
+docker system prune -a -f
 
 echo "Deployment complete!"
