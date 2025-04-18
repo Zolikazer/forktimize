853fcc705eb6e91dd22241da8a70165906695bf0
Zoltan Spagina <zoltan.spagina@nokia.com>
2025-04-18 11:43:34 +0200

    Return food vendors in camel case

--- backend/model/food_provider_data.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-from pydantic import BaseModel
-
-
-class FoodProviderData(BaseModel):
-    name: str
-    available_dates: list[date]
-    url: str
--- backend/model/food_vendor_data.py
+++ backend/model/food_vendor_data.py
@@ -1,9 +1,12 @@
 from datetime import date
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class FoodVendorData(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True, alias_generator=to_camel, populate_by_name=True)
+
     name: str
     menu_url: str
     available_dates: list[date]
