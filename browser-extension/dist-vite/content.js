54097bda8e7bf9bb9187df53686a8113d9547e9a
Zoltan Spagina <zoltan.spagina@nokia.com>
2025-08-16 20:46:55 +0200

    Remove old build artifacts from git tracking

--- browser-extension/dist-vite/content.js
+++
@@ -1,3 +0,0 @@
-import "./constants2.js";
-console.log("TypeScript content script loaded! 🔥");
-//# sourceMappingURL=content.js.map
--- browser-extension/dist-vite/popup.js
+++
@@ -1,3 +0,0 @@
-import "./constants2.js";
-console.log("TypeScript popup script loaded! 🔥");
-//# sourceMappingURL=popup.js.map
